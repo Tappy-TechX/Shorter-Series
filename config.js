4f0e5478a6bb3d6406ee446b42106650fb53ffb7
Tappy-TechX <tappyblack2@gmail.com>
2026-03-12 17:20:32 +0300

    Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     PORT: process.env.PORT || 50900,
     SESSION_PREFIX: process.env.SESSION_PREFIX || "Shadow-Xtech~",
     GC_JID: process.env.GC_JID || "CbUGTuJP03D1LJfK6wENCN",
-    DATABASE_URL: process.env.DATABASE_URL || "postgresql", // Your Db URL here(optional). Can either be mongodb or postreSQL
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_AeNhu3a2VFOM@ep-bitter-bird-an8g7zvq-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=verify-full&channel_binding=require", 
     BOT_REPO: process.env.BOT_REPO || "https://github.com/Tappy-TechX/Shadow-Xtech",
     WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VasHgfG4tRrwjAUyTs10",
     MSG_FOOTER: process.env.MSG_FOOTER || "> *Powered By Tappy-TechX*",
